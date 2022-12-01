b988846561914c55a929e0ca18aca67cdfcf799e
pta208VDT <pta20801@gmail.com>
2022-12-01 11:30:09 +0700

    Update _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,12 +5,17 @@ export default function Document() {
   return (
     <Html lang="vi">
       <Head>
+        <meta property="og:title" content="BVN Distribution" key="title" />
+        <meta property="og:url" content="https://phanphoibvn.vn/" />
+        <meta property="og:type" content="website" />
         <link href="https://fonts.googleapis.com" />
         <link href="https://fonts.gstatic.com" />
         <link
           href={`https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800;900&family=Roboto:wght@400;500;700;900&display=swap`}
           rel="stylesheet"
         />
+
+        <title>BVN Distribution</title>
       </Head>
       <body>
         <Main />
@@ -22,6 +27,16 @@ export default function Document() {
           nonce="B7oynq4a"
         ></Script>
         <Script async src="https://www.tiktok.com/embed.js\"></Script>
+
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-JPSZD5MK56"
+        ></Script>
+        <Script>
+          window.dataLayer = window.dataLayer || []; function gtag()
+          {dataLayer.push(arguments)}
+          gtag('js', new Date()); gtag('config', 'G-JPSZD5MK56');
+        </Script>
       </body>
     </Html>
   );
