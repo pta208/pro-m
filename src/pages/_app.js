1accc7f2f3e77e26c9425e816e58ab430894e198
pta208VDT <pta20801@gmail.com>
2023-04-17 14:59:16 +0700

    add meta pixel code

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -17,6 +17,18 @@ const theme = extendTheme({
 function MyApp({ Component, pageProps }) {
   const Layout = Component.Layout ? Component.Layout : LandingPageLayout;
   const title = Component.Title;
+  useEffect(() => {
+    import("react-facebook-pixel")
+      .then((x) => x.default)
+      .then((ReactPixel) => {
+        ReactPixel.init("237423865477943");
+        ReactPixel.pageView();
+
+        router.events.on("routeChangeComplete", () => {
+          ReactPixel.pageView();
+        });
+      });
+  }, [router.events]);
   return (
     <ChakraProvider theme={theme}>
       <Meta title={title && title} />
